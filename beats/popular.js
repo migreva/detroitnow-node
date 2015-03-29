736c3f5a50dcd51d3af4f2992d01df42361560bf
Eric Bower <neurosnap@gmail.com>
2015-03-28 22:34:26 -0400

    coroutines baby

--- beats/popular.js
+++ beats/popular.js
@@ -1,11 +1,14 @@
-var helper = require('./helper');
-var config = require('../config');
 var _ = require('lodash');
+var moment = require('moment');
 var Promise = require('bluebird');
 var needle = Promise.promisifyAll(require('needle'));
 
+var config = require('../config');
+var helper = require('./helper');
+
 module.exports = function(app) {
-  var connections = [];
+  'use strict';
+
   app.get('/', function(req, res, next) {
     res.render('popular', { title: 'Popular Articles' });
   });
@@ -14,60 +17,66 @@ module.exports = function(app) {
     req.io.join('popular')
   });
 
-  function fetchData() {
+  var chartbeat_template = _.template("<%= chartbeat_url %>/live/toppages/v3/?limit=50&apikey=<%= api_key %>&host=")
+  var chartbeat_string = chartbeat_template({
+    chartbeat_url: config.chartbeat_url,
+    api_key: config.api_key
+  });
+
+  var urls = [];
+  _.forEach(config.sites, function(site) {
+    urls.push(chartbeat_string + site);
+  });
+
+  // Required to handle an array of promises
+  // https://github.com/petkaantonov/bluebird/blob/master/API.md#promisecoroutineaddyieldhandlerfunction-handler---void
+  Promise.coroutine.addYieldHandler(function(yieldedValue) {
+    if (Array.isArray(yieldedValue)) return Promise.all(yieldedValue);
+  });
+
+  // Coroutine = generator + promises
+  // https://github.com/petkaantonov/bluebird/blob/master/API.md#generators
+  var fetchData = Promise.coroutine(function* () {
     if (!app.io.sockets.clients('popular').length) {
       setTimeout(fetchData, 5000);
+      console.log(moment() + ": no clients connected to the popular dashboard, ignoring request")
       return;
     }
-    // Format the URLs
-    console.log("Fetching popular");
-    var chartbeat_template = _.template("<%= chartbeat_url %>/live/toppages/v3/?limit=50&apikey=<%= api_key %>&host=")
-    var chartbeat_string = chartbeat_template({
-      chartbeat_url: config.chartbeat_url,
-      api_key: config.api_key
-    });
 
-    var urls = [];
-    _.forEach(config.sites, function(site) {
-      urls.push(chartbeat_string + site);
-    });
+    console.log(moment() + ": fetching popular data");
 
+    var promises = [];
     var articles = [];
-    var current = Promise.resolve();
-    Promise.map(urls, function(URL) {
-      current = current.then(function() {
-        return needle.getAsync(URL);
-      });
-      return current;
-    }).map(function(res) {
-      var articles = [];
-      //console.log(res[1]);
-      _.forEach(res[1].pages, function(article) {
-        if (helper.isSectionPage(article.path)) {
-          return;
-        }
+
+    _.forEach(urls, function(url) {
+      promises.push(needle.getAsync(url));
+    });
+
+    // send requests to chartbeat in parallel
+    try {
+      var responses = yield promises;
+    } catch (e) {
+      console.log(moment() + ": " + e);
+    }
+
+    _.forEach(responses, function(response) {
+      _.forEach(response[1].pages, function(article) {
+        if (helper.isSectionPage(article.path)) return;
+
         articles.push({
           path: article.path,
           title: article.title,
           visits: article.stats.visits
         });
       });
+    });
 
-      return articles;
-    }).then(function(articles) {
-      articles = _.sortByOrder(_.flatten(articles), ['visits'], false);
-
-      app.io.room('popular').broadcast('chartbeat', {
-          articles: articles.splice(0, 40)
-      });
-    }).then(function() {
-      console.log('All needle requests saved!')
-    }).catch(function(e) {
-      console.log(e);
+    app.io.room('popular').broadcast('chartbeat', {
+      articles: articles.splice(0, 40)
     });
 
     setTimeout(fetchData, 5000);
-  }
+  });
 
   fetchData();
 }
